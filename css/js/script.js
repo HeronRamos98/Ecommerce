3ac23bb9635394072ea5f417b7a0ef66856f5c85
Heron Ramos <heron@cooperalfa.local>
2024-09-23 15:10:11 -0300

    Criando arquivos JSON e consumindo-os através de Java Script

--- css/js/script.js
+++ css/js/script.js
@@ -1,4 +1,4 @@
-
+//*Scrpit Dropdown
 window.onclick = function(event) {
   if (!event.target.matches('.dropdown-link')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -11,3 +11,29 @@ window.onclick = function(event) {
     }
   }
 }
+//*Scrpit consumindo arquivo JSON(nome do arquivo : masc.jason)
+fetch('masc.json')
+.then(response => response.json())
+.then(data => {
+    const container = document.getElementById('produto-container');
+    let produtosHtml = '';
+
+    //*Relacionar os produtos e gerá-los no HTML
+    data.forEach(produto => {
+        produtosHtml += `
+            <div class="produto">
+                <img src="${produto['img-capa']}" alt="${produto['nome-produto']}">
+                <h3>${produto['nome-produto']}</h3>
+                <p><strong>Marca:</strong> ${produto.marca}</p>
+                <p><strong>Categoria:</strong> ${produto.categoria}</p>
+                <p><strong>Valor:</strong> R$ ${produto.valor}</p>
+            </div>
+        `;
+    });
+
+    //*Inserindo produtos no container
+    container.innerHTML = produtosHtml;
+})
+.catch(error => {
+    console.error('Erro ao carregar o arquivo JSON:', error);
+});
